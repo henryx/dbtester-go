48e90c8c92da90ee9a3e1898e39b034cd46acef3
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-04-23 22:44:58 +0200

    Started project

---
+++ dbtester.go
@@ -0,0 +1,14 @@
+/*
+Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       dbtester
+Description   A simple database tester
+License       GPL version 2 (see GPL.txt for details)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
