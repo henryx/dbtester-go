a5b56c4073d9beab48ab72202c528abd103ac2fc
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-04-23 23:14:08 +0200

    Added package

---
+++ src/database/mysql.go
@@ -0,0 +1,8 @@
+/*
+Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       dbtester
+Description   A simple database tester
+License       GPL version 2 (see GPL.txt for details)
+*/
+
+package database
