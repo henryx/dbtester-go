671b712a87cb8fbd770e81f2ee80a5eea8872e18
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-04-24 18:04:44 +0200

    Extendend common function and specified function

--- src/db/common.go
+++ src/db/common.go
@@ -7,12 +7,21 @@ License       GPL version 2 (see GPL.txt for details)
 
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
-func checkMySQLStructure(db *sql.DB) bool {
+func checkMySQLStructure(db *sql.DB, dbname string) bool {
 	return true
 }
 
-func CheckStructure(db *sql.DB) bool {
-	return checkMySQLStructure(db)
+func CheckStructure(db *sql.DB, dbname, engine string) (bool, error) {
+	switch engine {
+	case "mysql":
+		return checkMySQLStructure(db, dbname), nil
+	default:
+		return nil, errors.New("Unknown database engine: " + engine)
+	}
+
 }
